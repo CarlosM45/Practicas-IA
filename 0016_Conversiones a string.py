4b6ab0b8201e92e109d975ea1e7a5d974bff9eb3
CarlosM45 <143459593+CarlosM45@users.noreply.github.com>
2025-03-07 02:44:26 +0000

    practica conversiones 2

---
+++ 0016_Conversiones a string.py
@@ -0,0 +1,5 @@
+#Además de convertir cadenas de caracteres en valores, también se puede realizar lo contrario
+#A continuación, el programa de la hipotenusa una vez más pero ahora se puede evitar el uso de comas con lo aprendido
+cat_a=float(input("Ingrese el valor del primer cateto\n"))
+cat_b=float(input("Ingrese el valor del segundo cateto\n"))
+print("El valor de la hipotenusa es:"+str((cat_a**2+cat_b**2)**0.5))#esta vez salté también la declaración de una variable para almacenar la hipotenusa
