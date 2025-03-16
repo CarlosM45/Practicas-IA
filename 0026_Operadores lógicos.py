88bde28dc51dceb5073401ab5c02a5d4efcabf63
CarlosM45 <carlosalej2015@gmail.com>
2025-03-16 11:31:19 -0600

    Create 0026_Operadores lógicos.py

---
+++ 0026_Operadores lógicos.py
@@ -0,0 +1,6 @@
+# El operador and realiza una multiplicación, o lo que es más fácil, pide que se cumplan ambas condiciones para devolver True
+print(True and True)
+# El operador or realiza una suma, o requiere que una o la otra de las condiciones sea cierta
+print(True or False)
+# El operador not niega la condición, efectivamente invirtiendo el 0 a 1 y el 1 al 0
+print(not False)
