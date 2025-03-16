9819abcee87085bde5a8f22af36cb5edac268eed
CarlosM45 <carlosalej2015@gmail.com>
2025-03-16 11:02:13 -0600

    Update 0024_Break y continue.py
    terminado

--- 0024_Break y continue.py
+++ 0024_Break y continue.py
@@ -1,12 +1,16 @@
-#Para finalizar un bucle prematuramente o saltar una parte del mismo, Python tiene break y continue a nuestra disposición
-#Bucle con break si el usuario escribe "Python"
+# Para finalizar un bucle prematuramente o saltar una parte del mismo, Python tiene break y continue a nuestra disposición
+# Bucle con break si el usuario escribe "Python"
 while True:
-    lenguaje=input("¿Cuál es el mejor lenguaje de programación?\n")
-    if lenguaje=="Python":
+    lenguaje = input("¿Cuál es el mejor lenguaje de programación?\n")
+    if lenguaje == "Python":
         print("Cierto")
         break
 print("Eres libre, por ahora")
-#Bucle con continue si el usuario no escribe "Python"
-lenguaje=input("¿Cuál es el mejor lenguaje de programación?\n")
-while lenguaje!="Python":
-    
+# Bucle con continue si el usuario escribe "Python"
+lenguaje = input("¿Cuál es el mejor lenguaje de programación?\n")
+while lenguaje != "Python":
+    if lenguaje == "Python":
+        continue
+    print("Incorrecto")
+    lenguaje = input("¿Cuál es el mejor lenguaje de programación?\n")
+print("Cierto, bye")
