7209c6b7ba4b097cea642ad6a827038596814bdf
CarlosM45 <carlosalej2015@gmail.com>
2025-03-16 12:25:00 -0600

    Create 0029_In y not in.py

---
+++ 0029_In y not in.py
@@ -0,0 +1,6 @@
+# Los operadores in y not in comparan un valor con una lista, para devolver si se encuentra o no el elemento, respectivamente
+colores_favoritos = ["Rojo", "Verde", "Azul"]
+# Experimentamos con lo dicho anteriormente
+print("Rojo" in colores_favoritos)
+print("Morado" in colores_favoritos)
+print("Azul" not in colores_favoritos)
